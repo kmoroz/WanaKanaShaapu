82f2d474a2ac4519007eb207c0d47f93bcb6a2df
kmoroz <ksmorozova@hotmail.com>
2022-07-18 18:55:04 +0100

    initial commit

---
+++ WanaKanaSharp.UnitTest/ToHiraganaUnitTests.cs
@@ -0,0 +1,21 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WanaKanaSharp.UnitTests
+{
+    [TestFixture]
+    public class ToHiraganaUnitTests
+    {
+        [TestCase("toukyou, オオサカ", "とうきょう、　おおさか")]
+        public void ToHiragana_WhenPassedKatakanaAndRomaji_ReturnsThemConvertedToHiragana(string input, string expectedOutput)
+        {
+            string result = WanaKana.ToHiragana(input);
+
+            Assert.AreEqual(result, expectedOutput);
+        }
+    }
+}
