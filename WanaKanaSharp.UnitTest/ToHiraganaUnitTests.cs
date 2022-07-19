a26278a5f1509334b1603eaff7716090661799a1
kmoroz <ksmorozova@hotmail.com>
2022-07-19 15:08:21 +0100

    add test cases for optional flags

--- WanaKanaSharp.UnitTest/ToHiraganaUnitTests.cs
+++ WanaKanaSharp.UnitTest/ToHiraganaUnitTests.cs
@@ -11,9 +11,26 @@ namespace WanaKanaSharp.UnitTests
     public class ToHiraganaUnitTests
     {
         [TestCase("toukyou, オオサカ", "とうきょう、　おおさか")]
+        [TestCase("wi", "うぃ")]
         public void ToHiragana_WhenPassedKatakanaAndRomaji_ReturnsThemConvertedToHiragana(string input, string expectedOutput)
         {
             string result = WanaKana.ToHiragana(input);
+
+            Assert.AreEqual(result, expectedOutput);
+        }
+
+        [TestCase("only カナ", true, "only かな")]
+        public void ToHiragana_WhenPassRomajiFlagIsEngabled_OnlyConvertsKatakana(string input, bool passRomaji, string expectedOutput)
+        {
+            string result = WanaKana.ToHiragana(input, new DefaultOptions { PassRomaji = passRomaji });
+
+            Assert.AreEqual(result, expectedOutput);
+        }
+
+        [TestCase("wi", true, "ゐ")]
+        public void ToHiragana_WhenPassUseObsoleteKanaFlag_ReturnsObsoleteKana(string input, bool useObsoleteKana, string expectedOutput)
+        {
+            string result = WanaKana.ToHiragana(input, new DefaultOptions { UseObsoleteKana = useObsoleteKana });
 
             Assert.AreEqual(result, expectedOutput);
         }
