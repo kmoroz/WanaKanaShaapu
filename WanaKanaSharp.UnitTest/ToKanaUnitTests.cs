92878badf85176a5b4f2c37b637e26b967d008f6
kmoroz <ksmorozova@hotmail.com>
2022-07-26 17:03:41 +0100

    initial

---
+++ WanaKanaSharp.UnitTest/ToKanaUnitTests.cs
@@ -0,0 +1,25 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WanaKanaSharp.UnitTests
+{
+    [TestFixture]
+    public class ToKanaUnitTests
+    {
+        [TestCase("onaji BUTTSUUJI", "おなじ ブッツウジ")]
+        [TestCase("ONAJI buttsuuji", "オナジ ぶっつうじ")]
+        [TestCase("座禅‘zazen’スタイル", "座禅「ざぜん」スタイル")]
+        [TestCase("batsuge-mu", "ばつげーむ")]
+        [TestCase("!?.:/,~-‘’“”[](){}", "！？。：／、〜ー「」『』［］（）｛｝")]
+        public void ToKana_WhenPassedLowerAndUpperCaseLatinChacters_ReturnsThemConvertedToHiraganaAndKatakanaRespectively(string input, string expectedOutput)
+        {
+            string result = WanaKana.ToKana(input);
+
+            Assert.AreEqual(result, expectedOutput);
+        }
+    }
+}
---
+++ WanaKanaSharp.UnitTest/ToKatakanaUnitTests.cs
@@ -0,0 +1,38 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WanaKanaSharp.UnitTests
+{
+    [TestFixture]
+    public class ToKatakanaUnitTests
+    {
+        [TestCase("toukyou, おおさか", "トウキョウ、　オオサカ")]
+        [TestCase("wi", "ウィ")]
+        public void ToKatakana_WhenPassedHiraganaAndRomaji_ReturnsThemConvertedToKatakana(string input, string expectedOutput)
+        {
+            string result = WanaKana.ToKatakana(input);
+
+            Assert.AreEqual(result, expectedOutput);
+        }
+
+        [TestCase("only かな", true, "only カナ")]
+        public void ToKatakana_WhenPassRomajiFlagIsEngabled_OnlyConvertsHiragana(string input, bool passRomaji, string expectedOutput)
+        {
+            string result = WanaKana.ToKatakana(input, new DefaultOptions { PassRomaji = passRomaji });
+
+            Assert.AreEqual(result, expectedOutput);
+        }
+
+        [TestCase("wi", true, "ヰ")]
+        public void ToKatakana_WhenPassUseObsoleteKanaFlag_ReturnsObsoleteKana(string input, bool useObsoleteKana, string expectedOutput)
+        {
+            string result = WanaKana.ToKatakana(input, new DefaultOptions { UseObsoleteKana = useObsoleteKana });
+
+            Assert.AreEqual(result, expectedOutput);
+        }
+    }
+}
