fb2d22f086e7b85a7ccb0eae46e327a6f4a4a457
kmoroz <ksmorozova@hotmail.com>
2022-07-27 16:34:22 +0100

    edit default options to match the official documentaion

--- WanaKanaSharp/DefaultOptions.cs
+++ WanaKanaSharp/DefaultOptions.cs
@@ -10,9 +10,11 @@ namespace WanaKanaSharp
     {
         public bool UseObsoleteKana { get; set; }
         public bool PassRomaji { get; set; }
-        public bool PassKanji { get; set; } = true;
+        public bool ConvertLongVowelMark { get; set; } = true;
         public bool UpcaseKatakana { get; set; }
         public dynamic IMEMode { get; set; }
         public string Romanization { get; set; } = "hepburn";
+        public Dictionary<string, string> CustomKanaMapping = new();
+        public Dictionary<string, string> CustomRomajiMapping = new();
     }
 }
