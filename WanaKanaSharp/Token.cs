d175e62300936bdc457fdacebdd2cb59b6f1a74d
kmoroz <ksmorozova@hotmail.com>
2022-08-02 16:20:33 +0100

    initial

---
+++ WanaKanaSharp/Token.cs
@@ -0,0 +1,22 @@
+﻿using System.Linq;
+
+namespace WanaKanaSharp
+{
+    public struct Token
+    {
+        public string Type { get; set; }
+        public string Value { get; set; }
+
+        public Token(string type, string value)
+        {
+            Type = type;
+            Value = value;
+        }
+    }
+
+    public class Tokenization
+    {
+        public List<Token> Tokens { get; set; } = new List<Token>();
+        public string[] Values { get { return Tokens.Select(token => token.Value).ToArray(); } }
+    }
+}
