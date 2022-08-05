3f913dd04b5d41064ae1724ca670636a5eb296b0
kmoroz <ksmorozova@hotmail.com>
2022-08-05 14:53:09 +0100

    initial

---
+++ WanaKanaSharp.UnitTest/ToRomajiUnitTests.cs
@@ -0,0 +1,30 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WanaKanaSharp.UnitTests
+{
+    [TestFixture]
+    public class ToRomajiUnitTests
+    {
+        [TestCase("ひらがな　カタカナ", "hiragana katakana")]
+        //[TestCase("げーむ　ゲーム", "geemu ge-mu")]
+        public void ToRomaji_WhenPassedKana_ReturnsItConvertedToLatinCharacters(string input, string expectedOutput)
+        {
+            string result = WanaKana.ToRomaji(input);
+
+            Assert.AreEqual(result, expectedOutput);
+        }
+
+        [TestCase("ひらがな カタカナ", true, "hiragana KATAKANA")]
+        public void ToRomaji_WhenPassedUpcaseKatakanaTrue_ReturnsUppercaseLatinCharacters(string input, bool upcaseKatakana, string expectedOutput)
+        {
+            string result = WanaKana.ToRomaji(input, new DefaultOptions { UpcaseKatakana = upcaseKatakana });
+
+            Assert.AreEqual(result, expectedOutput);
+        }
+    }
+}
