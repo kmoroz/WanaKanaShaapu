39b38235e822ba1f44c0f47bfc38979481ee016a
kmoroz <ksmorozova@hotmail.com>
2022-08-30 15:52:01 +0100

    split Token struct and Tokenization class into separate files

--- WanaKanaSharp/Token.cs
+++ WanaKanaSharp/Token.cs
@@ -1,20 +1,14 @@
 ﻿namespace WanaKanaSharp
 {
-    public struct Token
+    readonly public struct Token
     {
-        public string Type { get; set; }
-        public string Value { get; set; }
+        readonly public string Type { get; }
+        readonly public string Value { get; }
 
         public Token(string type, string value)
         {
             Type = type;
             Value = value;
         }
-    }
-
-    public class Tokenization
-    {
-        public List<Token> Tokens { get; set; } = new List<Token>();
-        public string[] Values { get { return Tokens.Select(token => token.Value).ToArray(); } }
     }
 }
