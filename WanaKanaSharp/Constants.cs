3f532cd234d4d7042070af8197c5837c17f58d99
kmoroz <ksmorozova@hotmail.com>
2022-08-30 15:54:48 +0100

    add regions

--- WanaKanaSharp/Constants.cs
+++ WanaKanaSharp/Constants.cs
@@ -2,40 +2,42 @@
 {
     public class Constants
     {
-        public static char KanaMin = '\u3040';
-        public static char KanaMax = '\u30ff';
-        public static char PunctuationMin = '\u3000';
-        public static char PunctuationMax = '\u303f';
-        public static char Choonpu = 'ー';
-        public static char KanjiMin = '\u4e00';
-        public static char KanjiMax = '\u9fa0';
-        public static char RomajiMin = '\uff20';
-        public static char RomajiMax = '\uff50';
-        public static char JapanesePunctuationMin = '\u3000';
-        public static char JapanesePunctuationMax = '\u303f';
-        public static char ZenzakuMin = '\uff00';
-        public static char ZenzakuMax = '\uffef';
-        public static string EnglishVowels = "aeiouAEIOU";
-        public static string SokuonHiragana = "っ";
-        public static string SokuonKatakana = "ッ";
+        #region character ranges
 
-        public static CharacterRange HiraganaChars = new CharacterRange('\u3040', '\u309f');
-        public static CharacterRange KatakanaChars = new CharacterRange('\u30a0', '\u30ff');
-        public static CharacterRange KanjiChars = new CharacterRange('\u4e00', '\u9fa0');
-        public static CharacterRange ZenkakuNumbers = new CharacterRange('\uff10', '\uff19');
-        public static CharacterRange ZenkakuUppercaseChars = new CharacterRange('\uff21', '\uff3a');
-        public static CharacterRange ZenkakuLowercaseChars = new CharacterRange('\uff41', '\uff5a');
-        public static CharacterRange ZenkakuCurrencyChars = new CharacterRange('\uffe0', '\uffee');
-        public static CharacterRange CJKPunctuationChars = new CharacterRange('\u3000', '\u303f');
-        public static CharacterRange RomajiChars = new CharacterRange('\u0000', '\u007f');
-        public static CharacterRange ZenkakuPunct1 = new CharacterRange('\uff01', '\uff0f');
-        public static CharacterRange ZenkakuPunct2 = new CharacterRange('\uff1a', '\uff1f');
-        public static CharacterRange ZenkakuPunct3 = new CharacterRange('\uff3b', '\uff3f');
-        public static CharacterRange ZenkakuPunct4 = new CharacterRange('\uff5b', '\uff60');
-        public static CharacterRange KanaPunctChars = new CharacterRange('\uff61', '\uff65');
-        public static CharacterRange KatakanaPunctChars = new CharacterRange('\u30fb', '\u30fc');
+        public const char KanaMin = '\u3040';
+        public const char KanaMax = '\u30ff';
+        public const char PunctuationMin = '\u3000';
+        public const char PunctuationMax = '\u303f';
+        public const char Choonpu = 'ー';
+        public const char KanjiMin = '\u4e00';
+        public const char KanjiMax = '\u9fa0';
+        public const char RomajiMin = '\uff20';
+        public const char RomajiMax = '\uff50';
+        public const char JapanesePunctuationMin = '\u3000';
+        public const char JapanesePunctuationMax = '\u303f';
+        public const char ZenzakuMin = '\uff00';
+        public const char ZenzakuMax = '\uffef';
+        public const string EnglishVowels = "aeiouAEIOU";
+        public const string SokuonHiragana = "っ";
+        public const string SokuonKatakana = "ッ";
 
-        public static CharacterRange[] JapanesePunctuation = new CharacterRange[]
+        readonly public static CharacterRange HiraganaChars = new CharacterRange('\u3040', '\u309f');
+        readonly public static CharacterRange KatakanaChars = new CharacterRange('\u30a0', '\u30ff');
+        readonly public static CharacterRange KanjiChars = new CharacterRange('\u4e00', '\u9fa0');
+        readonly public static CharacterRange ZenkakuNumbers = new CharacterRange('\uff10', '\uff19');
+        readonly public static CharacterRange ZenkakuUppercaseChars = new CharacterRange('\uff21', '\uff3a');
+        readonly public static CharacterRange ZenkakuLowercaseChars = new CharacterRange('\uff41', '\uff5a');
+        readonly public static CharacterRange ZenkakuCurrencyChars = new CharacterRange('\uffe0', '\uffee');
+        readonly public static CharacterRange CJKPunctuationChars = new CharacterRange('\u3000', '\u303f');
+        readonly public static CharacterRange RomajiChars = new CharacterRange('\u0000', '\u007f');
+        readonly public static CharacterRange ZenkakuPunct1 = new CharacterRange('\uff01', '\uff0f');
+        readonly public static CharacterRange ZenkakuPunct2 = new CharacterRange('\uff1a', '\uff1f');
+        readonly public static CharacterRange ZenkakuPunct3 = new CharacterRange('\uff3b', '\uff3f');
+        readonly public static CharacterRange ZenkakuPunct4 = new CharacterRange('\uff5b', '\uff60');
+        readonly public static CharacterRange KanaPunctChars = new CharacterRange('\uff61', '\uff65');
+        readonly public static CharacterRange KatakanaPunctChars = new CharacterRange('\u30fb', '\u30fc');
+
+        readonly public static CharacterRange[] JapanesePunctuation = new CharacterRange[]
         {
             ZenkakuPunct1,
             ZenkakuPunct2,
@@ -47,7 +49,7 @@
             KatakanaPunctChars
         };
 
-        public static CharacterRange[] MacronChars = new CharacterRange[]
+        readonly public static CharacterRange[] MacronChars = new CharacterRange[]
         {
             new('\u0100', '\u0101'),
             new('\u0112', '\u0113'),
@@ -56,15 +58,13 @@
             new('\u016A', '\u016B')
         };
 
-
-
-        public static CharacterRange[] KanaChars = new CharacterRange[]
+        readonly public static CharacterRange[] KanaChars = new CharacterRange[]
         {
             HiraganaChars,
             KatakanaChars
         };
 
-        public static CharacterRange[] JapaneseChars = new CharacterRange[]
+        readonly public static CharacterRange[] JapaneseChars = new CharacterRange[]
         {
             HiraganaChars,
             KatakanaChars,
@@ -82,7 +82,11 @@
             KatakanaPunctChars
         };
 
-        public static readonly Dictionary<string, string> RomajiHiraganaDictionary = new Dictionary<string, string>()
+        #endregion
+
+        #region dictionaries
+
+        readonly public static Dictionary<string, string> RomajiHiraganaDictionary = new Dictionary<string, string>()
         {
             {"a", "あ"},
             {"i", "い"},
@@ -271,7 +275,7 @@
             {"fyo", "ふょ" }
         };
 
-        public static readonly Dictionary<string, string> RomajiKatakanaDictionary = new Dictionary<string, string>()
+        readonly public static Dictionary<string, string> RomajiKatakanaDictionary = new Dictionary<string, string>()
         {
             {"a", "ア"},
             {"i", "イ"},
@@ -469,7 +473,7 @@
             {"fyo", "フョ"}
         };
 
-        public static readonly Dictionary<string, string> WhitespacePunctuationDictionary = new Dictionary<string, string>()
+        readonly public static Dictionary<string, string> WhitespacePunctuationDictionary = new Dictionary<string, string>()
         {
             {" ", "　"},
             {"!", "！"},
@@ -508,16 +512,18 @@
             {"]", "］"}
         };
 
-        public static readonly Dictionary<string, string> RomajiObsoleteHiraganaDictionary = new Dictionary<string, string>()
+        readonly public static Dictionary<string, string> RomajiObsoleteHiraganaDictionary = new Dictionary<string, string>()
         {
             {"wi", "ゐ"},
             {"we", "ゑ" }
         };
 
-        public static readonly Dictionary<string, string> RomajiObsoleteKatakanaDictionary = new Dictionary<string, string>()
+        readonly public static Dictionary<string, string> RomajiObsoleteKatakanaDictionary = new Dictionary<string, string>()
         {
             {"wi", "ヰ"},
             {"we", "ヱ" }
         };
+
+        #endregion
     }
 }
