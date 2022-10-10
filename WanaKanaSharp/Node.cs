cb73ea6514743687e9a58b507c164a47a8288227
kmoroz <ksmorozova@hotmail.com>
2022-10-10 21:48:01 +0100

    initial

---
+++ WanaKanaSharp/Node.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WanaKanaSharp
+{
+    public class Node
+    {
+        public string Data { get; set; } = string.Empty;
+        public Dictionary<string, Node> Children { get; set; } = new();
+
+        public Node? FindChild(string key)
+        {
+            if (!Children.Any())
+                return null;
+
+            Children.TryGetValue(key, out Node child);
+
+            return child;
+        }
+
+        public Node(string data)
+        {
+            Data = data;
+            Children = new Dictionary<string, Node>();
+        }
+
+        public Node(string data, Dictionary<string, Node> children)
+        {
+            Data = data;
+            Children = children;
+        }
+    }
+}
