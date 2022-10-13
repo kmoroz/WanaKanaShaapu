af82a899d8ba1cee8f3e77c1796ea17d03c6da26
kmoroz <ksmorozova@hotmail.com>
2022-10-13 14:14:03 +0100

    add more test cases

--- WanaKanaSharp.UnitTest/ToKanaUnitTests.cs
+++ WanaKanaSharp.UnitTest/ToKanaUnitTests.cs
@@ -5,12 +5,33 @@ namespace WanaKanaSharp.UnitTests
     [TestFixture]
     public class ToKanaUnitTests
     {
-        [TestCase("onaji BUTTSUUJI", "おなじ ブッツウジ")]
-        [TestCase("ONAJI buttsuuji", "オナジ ぶっつうじ")]
         [TestCase("座禅‘zazen’スタイル", "座禅「ざぜん」スタイル")]
         [TestCase("batsuge-mu", "ばつげーむ")]
         [TestCase("!?.:/,~-‘’“”[](){}", "！？。：／、〜ー「」『』［］（）｛｝")]
+        [TestCase("!?.:/,~-‘’“”[](){}", "！？。：／、〜ー「」『』［］（）｛｝")]
+        public void ToKana_WhenPassedInput_ConvertsItCorrectly(string input, string expectedOutput)
+        {
+            string result = WanaKana.ToKana(input);
 
+            Assert.AreEqual(result, expectedOutput);
+        }
+
+        [TestCase("ヶ", "ヶ")]
+        [TestCase("ヵ", "ヵ")]
+        [TestCase("1", "1")]
+        [TestCase("@", "@")]
+        [TestCase("#", "#")]
+        [TestCase("$", "$")]
+        [TestCase("%", "%")]
+        public void ToKana_WhenPassedTheSymbolsThatAreNotToBeConverted_ReturnsThemAsTheyAre(string input, string expectedOutput)
+        {
+            string result = WanaKana.ToKana(input);
+
+            Assert.AreEqual(result, expectedOutput);
+        }
+
+        [TestCase("onaji BUTTSUUJI", "おなじ ブッツウジ")]
+        [TestCase("ONAJI buttsuuji", "オナジ ぶっつうじ")]
         public void ToKana_WhenPassedLowerAndUpperCaseLatinChacters_ReturnsThemConvertedToHiraganaAndKatakanaRespectively(string input, string expectedOutput)
         {
             string result = WanaKana.ToKana(input);
