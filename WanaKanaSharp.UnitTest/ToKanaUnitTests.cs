c360b620800c5fc6475b89f7effa0b0bfa52b0e7
kmoroz <ksmorozova@hotmail.com>
2022-11-09 16:17:22 +0000

    refactor some test cases

--- WanaKanaSharp.UnitTest/ToKanaUnitTests.cs
+++ WanaKanaSharp.UnitTest/ToKanaUnitTests.cs
@@ -7,8 +7,7 @@ namespace WanaKanaSharp.UnitTests
     {
         [TestCase("座禅‘zazen’スタイル", "座禅「ざぜん」スタイル")]
         [TestCase("batsuge-mu", "ばつげーむ")]
-        [TestCase("!?.:/,~-‘’“”[](){}", "！？。：／、〜ー「」『』［］（）｛｝")]
-        [TestCase("!?.:/,~-‘’“”[](){}", "！？。：／、〜ー「」『』［］（）｛｝")]
+        [TestCase("!?.:/,~-‘’“”[](){}", "！？。：・、〜ー「」『』［］（）｛｝")]
         public void ToKana_WhenPassedInput_ConvertsItCorrectly(string input, string expectedOutput)
         {
             string result = WanaKana.ToKana(input);
@@ -30,8 +29,8 @@ namespace WanaKanaSharp.UnitTests
             Assert.AreEqual(result, expectedOutput);
         }
 
-        [TestCase("onaji BUTTSUUJI", "おなじ ブッツウジ")]
-        [TestCase("ONAJI buttsuuji", "オナジ ぶっつうじ")]
+        [TestCase("onaji BUTTSUUJI", "おなじ　ブッツウジ")]
+        [TestCase("ONAJI buttsuuji", "オナジ　ぶっつうじ")]
         public void ToKana_WhenPassedLowerAndUpperCaseLatinChacters_ReturnsThemConvertedToHiraganaAndKatakanaRespectively(string input, string expectedOutput)
         {
             string result = WanaKana.ToKana(input);
