cbb28d280fa0bea175fe580ffabb511e54634b5f
kmoroz <ksmorozova@hotmail.com>
2022-11-16 13:25:34 +0000

    refactor TreeTraverser to handle bogus input

--- WanaKanaSharp/TreeTraverser.cs
+++ WanaKanaSharp/TreeTraverser.cs
@@ -15,29 +15,30 @@ namespace WanaKanaSharp
             //If character is not in tree, add and continue
             if (!tree.ContainsKey(word[0].ToString()))
                 return word[0].ToString() + TraverseTree(result, word[1..], head, tree, options);
-            
 
             Node node = tree[word[0].ToString()];
             if (options.CustomRomajiMapping.ContainsKey(word[0].ToString()))
                 return options.CustomRomajiMapping[word[0].ToString()] + TraverseTree(result, word[1..], tree, tree, options);
             
             if (word.Length == 1)
-                return node.Data;
+                return node.Data.Length == 0 && WanaKana.IsRomaji(word) 
+                    ? word : node.Data;
 
             if (WanaKana.IsHiragana(word.First().ToString()))
                 result = node.Data;
 
+            //おんly かな
             if (!node.Children.Any() || !node.Children.ContainsKey(word[1].ToString()))
-                result = node.Data;
-            if (!node.Children.Any() && word.Length == 1)
+                result = node.Data.Length > 0 ? node.Data : result += word[0];
+            else if (node.Data.Length == 0)
+                result += word[0].ToString();
+            else if (!node.Children.Any() && word.Length == 1)
                 return result;
 
             if (node.Children.ContainsKey(word[1].ToString()))
                 return TraverseTree(result, word[1..], head, tree[word[0].ToString()].Children, options);
             else
-                return result += TraverseTree(result, word[1..], head, head, options);
-
-            return string.Empty;
+                return result += TraverseTree(string.Empty, word[1..], head, head, options);
         }
     }
 }
